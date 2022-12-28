311a5b9cace95370a4cad8648f1548155945a6cf
thomasstrassmann <thomas_strassmann@freenet.de>
2022-12-28 15:42:29 +0000

    enable authentication and permissions

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -4,8 +4,13 @@ from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
 
     class Meta:
         model = Profile
-        fields = ['id', 'owner', 'username', 'avatar', 'email', 'phone',
-                  'created', 'updated']
+        fields = ['id', 'owner', 'is_owner', 'username', 'avatar', 'email',
+                  'phone', 'created', 'updated']
