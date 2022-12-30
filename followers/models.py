5a217390cd06d78679db53f07dd62a125d5f3867
thomasstrassmann <thomas_strassmann@freenet.de>
2022-12-30 13:21:48 +0000

    create follower model

--- followers/models.py
+++ followers/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Follower(models.Model):
+    """
+    followed_user and following_user are both model instances of User.
+    unique_together is necessary to prevent someone to follow the 
+    same user twice.
+    """
+    followed_user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='followed')
+    following_user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='following')
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created']
+        unique_together = ['following_user', 'followed_user']
+
+    def __str__(self):
+        return f'{self.following_user} follows {self.followed_user}'
