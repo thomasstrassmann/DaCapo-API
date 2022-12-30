7937194b2001c7bc643d96d460259194e740c3b9
thomasstrassmann <thomas_strassmann@freenet.de>
2022-12-30 10:42:27 +0000

    add custom bookmark model

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,3 +1,23 @@
+from instrument.models import Instrument
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Bookmark(models.Model):
+    """
+    The bookmark model is related to the User and Instrument model.
+    The meta property unique_together prevents a user from bookmarking
+    the same instrument more than once.
+    """
+    instrument = models.ForeignKey(
+        Instrument, related_name='bookmarks', on_delete=models.CASCADE
+    )
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created']
+        unique_together = ['instrument', 'owner']
+
+    def __str__(self):
+        return f'{self.owner} {self.instrument}'
