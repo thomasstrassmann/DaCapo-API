d9dd3220ef01e1e81f5d93634d27525e5cc650ec
thomasstrassmann <thomas_strassmann@freenet.de>
2022-12-31 15:59:54 +0000

    dj-rest-auth logout bugfix

--- dacapo_api/urls.py
+++ dacapo_api/urls.py
@@ -14,13 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from .views import root_route
+from .views import root_route, logout_route
 from django.urls import path, include
 
 urlpatterns = [
     path('', root_route),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('dj-rest-auth/logout/', logout_route),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/',
          include('dj_rest_auth.registration.urls')),
