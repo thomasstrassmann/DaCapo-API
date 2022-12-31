a287953ea0633036cac1ea44b2c7ff18eaedbe50
thomasstrassmann <thomas_strassmann@freenet.de>
2022-12-31 14:22:38 +0000

    add a goodlooking root url

--- dacapo_api/urls.py
+++ dacapo_api/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from .views import root_route
 from django.urls import path, include
 
 urlpatterns = [
+    path('', root_route),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
---
+++ dacapo_api/views.py
@@ -0,0 +1,17 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+@api_view()
+def root_route(request):
+    return Response({"introduction":
+                     "Hello and welcome to the dacapo-rest-api! You can "
+                     "access the following endpoints:",
+                     "profiles/": "GET",
+                     "profiles/id": "GET, PUT",
+                     "instruments/": "GET, POST",
+                     "instruments/id": "GET, PUT, DELETE",
+                     "bookmarks/": "GET, POST",
+                     "bookmarks/id": "GET, DELETE",
+                     "followers/": "GET, POST",
+                     "followers/id": "GET, DELETE"
+                     })
