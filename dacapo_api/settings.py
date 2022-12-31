36c1362228dbe7a7dac3f690e8f1e4ebca9d86fe
thomasstrassmann <thomas_strassmann@freenet.de>
2022-12-31 15:51:25 +0000

    bug fix: try another setting

--- dacapo_api/settings.py
+++ dacapo_api/settings.py
@@ -61,7 +61,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'https://dacapo-api.herokuapp.com/']
+ALLOWED_HOSTS = ['localhost', 'dacapo-api.herokuapp.com']
 
 
 # Application definition
