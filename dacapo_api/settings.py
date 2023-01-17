d8bb01d0def26945ebaf7c251b8ad7d7659e90ba
thomasstrassmann <thomas_strassmann@freenet.de>
2023-01-17 10:23:44 +0000

    bug fix, change settings back

--- dacapo_api/settings.py
+++ dacapo_api/settings.py
@@ -60,7 +60,7 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = ['localhost', os.environ.get('ALLOWED_HOST')]
 
