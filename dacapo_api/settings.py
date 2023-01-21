849a3c45f5b448b04e6c8c03dc0c5c81d60a15d4
thomasstrassmann <thomas_strassmann@freenet.de>
2023-01-21 11:50:22 +0000

    disable email verification

--- dacapo_api/settings.py
+++ dacapo_api/settings.py
@@ -52,6 +52,8 @@ JWT_AUTH_SAMESITE = 'None'
 REST_AUTH_SERIALIZERS = {
     'USER_DETAILS_SERIALIZER': 'dacapo_api.serializers.CurrentUserSerializer'}
 
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
